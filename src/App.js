1e9084d6a19e158901d1c24d28ce6f50a5e2e325
Shikhar Verma <shikhar1verma@gmail.com>
2023-01-10 21:25:29 +0530

    learning: How class based components are used previously instead of functional components, when to use them and what are error boundary and how to use them.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React, { useCallback, useState } from 'react';
-import Button from './components/UI/Button/Button';
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-
-  console.log('APP RUNNING');
-
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph(prevState => {
-        return !prevState;
-      })
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
+  const usersContext = {
+    users: DUMMY_USERS
   }
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
