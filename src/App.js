2d34aaff40bb05cd5d8da107154dea48fcc1bc70
Shikhar Verma <shikhar1verma@gmail.com>
2023-01-15 13:49:51 +0530

    learning: How actually in real case scenario like in http requests the custom hooks can be used.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState, useCallback } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  const transformTasks = useCallback((tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }, []);
+
+  useEffect(() => {
+    fetchTasks(
+      {
+        url: "https://react-custom-hooks-abf71-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks, transformTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks.bind(
+          null,
+          {
+            url: "https://react-custom-hooks-abf71-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+          },
+          transformTasks
+        )}
+      />
     </React.Fragment>
   );
 }
